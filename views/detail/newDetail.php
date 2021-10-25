da220398d56fffe348d6bf6db51f9116b64c6804
ThidaratPi <91516629+ThidaratPi@users.noreply.github.com>
2021-10-25 15:20:12 +0700

    Update newDetail.php

--- views/detail/newDetail.php
+++ views/detail/newDetail.php
@@ -1,15 +1,16 @@
 
 <form method="get" action="">
-<label>เลขบัตรประชาชน <input type="text" name="id_staff"/></label><br>
+<label>รหัส<input type="text" name="id_staff"/></label><br>
 <label>ชื่อจริง<input type="text" name="first_name"/></label><br>
 <label>นามสกุล <input type="text" name="last_name"/></label><br>
-<label>วันเกิด <input type="date" name="DOB"/></label><br>
-<label>เบอร์โทรศัพท์ <input type="text" name="phone"/></label><br>
-<label>ที่อยู่ <input type="text" name="address_staff"/></label><br>
+<label>วันเวลาจุดตรวจ <select name="name_customer">
+    <?php foreach($customer_List as $dep) {echo "<option value = $dep->id_customer>
+    $dep->name_customer</option>";}
+    ?>
+</select></label><br>
 
-
-<input type="hidden"name="controller"value="staff"/>
+<input type="hidden"name="controller"value="detail"/>
 <button type= "submit"name="action"value="index">back</button>
-<button type= "submit"name="action"value="addStaff">Save</button>
+<button type= "submit"name="action"value="addDetail">Save</button>
 
 </form>
